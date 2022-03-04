64cb6d58474e8535db16df467a8083b686b36da1
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2022-03-04 12:53:44 +0530

    Fix an issue with serializers

--- discounts/serializers.py
+++ discounts/serializers.py
@@ -7,10 +7,10 @@ class CampaignSerializer(serializers.ModelSerializer):
     class Meta:
         model = Campaign
         fields = ['id', 'discount_type', 'discount_rate', 'discount_amount', 'min_purchased_items',
-                  'apply_to', 'target_product', 'target_category', 'created_at', 'updated_at']
+                  'apply_to', 'target_product', 'target_category', 'created_at', 'updated_at', ]
 
 
 class CouponSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coupon
-        fields = ['id', 'min_cart_amount', 'discount_rate', 'created_at', 'updated_at']
+        fields = ['id', 'min_cart_amount', 'discount_rate', 'created_at', 'updated_at', ]
--- products/serializers.py
+++ products/serializers.py
@@ -4,10 +4,12 @@ from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    model = Category
-    fields = ['id', 'title', 'parent_category_id', 'created_at', 'updated_at', ]
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'parent_category_id', 'created_at', 'updated_at', ]
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    model = Product
-    fields = ['id', 'title', 'category', 'price', 'created_at', 'updated_at', ]
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'category', 'price', 'created_at', 'updated_at', ]
